b3b67a29187fd261c8d617fa3bd946633beb8f87
Ale <aledemark@gmail.com>
2023-04-06 19:20:14 +0200

    added .origin

---
+++ Mean/mean.c
@@ -0,0 +1,8 @@
+/** \file mean.c
+	\brief computing the mean valure
+	\author Alessandro Demarchi
+
+*/
+float mean(int a, int b) {
+	return(a + b) / 2;
+}
--- Mean/mean.h
+++ Mean/mean.h
@@ -1,1 +1,7 @@
-#pragma once
+/** \file mean.c
+	\brief computing the mean valure
+	\author Alessandro Demarchi
+
+*/
+#pragma once //serve a non includere due volte la libreria, once: una volta
+float mean(int a, int b);

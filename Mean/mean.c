e3f6b35d0e02696171957c7e53d3d0c5f83d302f
Ale <aledemark@gmail.com>
2023-04-06 20:45:56 +0200

    nothing

--- Mean/mean.c
+++ Mean/mean.c
@@ -1,8 +1,17 @@
 /** \file mean.c
-	\brief computing the mean valure
+	\brief computing the mean value of integers and complex integers
 	\author Alessandro Demarchi
 
 */
+
+#include "mean.h"
 float mean(int a, int b) {
-	return(a + b) / 2;
+	return(float)(a + b) / (float)2;
+}
+
+complexfloat complex_mean(complexint a, complexint b) {
+	complexfloat result;
+	result.re = mean(a.re,b.re);
+	result.im = mean(a.im,b.im);
+	return result;
 }

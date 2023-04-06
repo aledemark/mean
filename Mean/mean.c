687f834e4762134b7ef1737d542d73cfb2279365
Ale <aledemark@gmail.com>
2023-04-06 20:48:44 +0200

    nothing

--- Mean/mean.c
+++ Mean/mean.c
@@ -8,10 +8,3 @@
 float mean(int a, int b) {
 	return(float)(a + b) / (float)2;
 }
-
-complexfloat complex_mean(complexint a, complexint b) {
-	complexfloat result;
-	result.re = mean(a.re,b.re);
-	result.im = mean(a.im,b.im);
-	return result;
-}
